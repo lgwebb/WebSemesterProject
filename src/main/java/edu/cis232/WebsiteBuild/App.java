73f00bb178e3662c70fa70060400e2f22a9244a3
lucas webb <lucas@192.168.1.110>
2016-04-15 23:02:27 -0400

    First Commit.

---
+++ src/main/java/edu/cis232/WebsiteBuild/App.java
@@ -0,0 +1,13 @@
+package edu.cis232.WebsiteBuild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
